678e1f20ad8fc9917ce28a025e1fc9ca71a9c804
masintech <masintec@gmail.com>
2020-01-29 12:35:53 +0800

    LSP: explain in the code why we may need a factory

---
+++ LSP/build/CMakeFiles/LSP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LSP.dir/LSP.cpp.o"
+  "LSP"
+  "LSP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LSP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
