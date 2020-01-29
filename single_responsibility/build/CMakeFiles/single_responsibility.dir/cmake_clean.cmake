597eaac09fa9799dfd8d21d7b8b63562bcd40478
masintech <masintec@gmail.com>
2020-01-29 10:47:52 +0800

    SOLID principal: single_responsibility

---
+++ single_responsibility/build/CMakeFiles/single_responsibility.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/single_responsibility.dir/single_responsibility.cpp.o"
+  "single_responsibility"
+  "single_responsibility.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/single_responsibility.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
