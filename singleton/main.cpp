dbae6fdcbb6e825972344ce5652986b3a7b169fb
masintech <masintec@gmail.com>
2022-04-21 18:46:54 +0800

    add independent test and integration test

--- singleton/main.cpp
+++ singleton/main.cpp
@@ -7,15 +7,18 @@
 #include <string>
 #include <vector>
 
+class DataBase {
+ public:
+  virtual int get_population(const std::string  &name) = 0;
+};
 
-class SingletonDatabase {
+class SingletonDatabase : public DataBase {
   SingletonDatabase() {
     std::cout << "Initializing database" << std::endl;
 
     std::ifstream ifs("./capitals.txt");
     std::string s, s2;
     while (getline(ifs, s)) {
-      std::cout << "s: " << s << std::endl;
       getline(ifs, s2);
       int pop = boost::lexical_cast<int>(s2);
       capitals[s] = pop;
@@ -33,7 +36,21 @@ class SingletonDatabase {
     static SingletonDatabase db;
     return db;
   }
-  int get_population(const std::string& name) { return capitals[name]; }
+  int get_population(const std::string& name) override { return capitals[name]; }
+};
+
+class DummyDatabase : public DataBase {
+  std::map<std::string, int> capitals;
+
+ public:
+  DummyDatabase() {
+    capitals["alpha"] = 1;
+    capitals["beta"] = 2;
+    capitals["gamma"] = 3;
+  }
+  int get_population(const std::string& name) override {
+    return capitals[name];
+  }
 };
 
 struct SingletonRecordFinder {
@@ -45,15 +62,36 @@ struct SingletonRecordFinder {
   }
 };
 
-TEST(RecordFinderTests, SingletonTotalPopulationTest)
+struct ConfigurableRecordFinder
 {
-    SingletonRecordFinder rf;
-    std::vector<std::string> names{"Seoul", "Mexico City"};
-    int tp = rf.total_population(names);
-    EXPECT_EQ(3000+3000, tp);
+    DataBase& db;
+    ConfigurableRecordFinder(DataBase &db): db(db) {}
+    
+    int total_population(std::vector<std::string> names)
+    {
+        int result{0}; // new method for cpp initialization
+        for (auto & name : names)
+            result += db.get_population(name);
+        return result;
+    }
+};
+
+// Integration test
+TEST(RecordFinderTests, SingletonTotalPopulationTest) {
+  SingletonRecordFinder rf;
+  std::vector<std::string> names{"Seoul", "Mexico City"};
+  int tp = rf.total_population(names);
+  EXPECT_EQ(3000 + 3000, tp);
+}
+
+// Independent test
+TEST(RecordFinderTest, DependentTotalPopulationTest) {
+    DummyDatabase db;
+    ConfigurableRecordFinder rf {db};
+    EXPECT_EQ(4, rf.total_population(std::vector<std::string>{"alpha", "gamma"}));
 }
 
 int main(int ac, char** av) {
-    testing::InitGoogleTest(&ac, av);
-    return RUN_ALL_TESTS();
+  testing::InitGoogleTest(&ac, av);
+  return RUN_ALL_TESTS();
 }
