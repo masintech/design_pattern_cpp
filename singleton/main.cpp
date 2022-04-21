2231aa650f7f62b44d2b3dc8d4bd1e25a34222e1
masintech <masintec@gmail.com>
2022-04-21 11:51:44 +0800

    environment setting

---
+++ singleton/main.cpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <boost/lexical_cast.hpp>
+#include <vector>
+
+int main(int, char**) {
+    std::cout << "Hello, world!\n";
+}
